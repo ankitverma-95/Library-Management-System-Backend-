96f54efa95284cf58fa4d9fb2e6f5e64a9ca3a48
Ankit Verma <av6007616@gmail.com>
2024-11-25 23:44:52 +0530

    Bcrypt password encoder has added

--- src/main/java/com/rbac/vrv/service/UsersService.java
+++ src/main/java/com/rbac/vrv/service/UsersService.java
@@ -3,6 +3,7 @@ package com.rbac.vrv.service;
 import com.rbac.vrv.modal.Users;
 import com.rbac.vrv.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,10 @@ public class UsersService {
     @Autowired
     private UserRepo userRepo;
 
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
     public Users register(Users user) {
+        user.setPassword(encoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 
